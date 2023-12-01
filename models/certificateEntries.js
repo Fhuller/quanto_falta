43e93a9e603755298ff4eb60010ec085ac24ecc6
Fhuller <fhullerbr@gmail.com>
2023-12-01 00:22:13 -0300

    tirando todos os min de var

--- models/certificateEntries.js
+++ models/certificateEntries.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let certificateEntrieSchema = new Schema({
-  name: { type: String, required: true, min: 6, max: 255 },
-  description: { type: String, required: true, min: 6, max: 255 },
-  file: { type: String, required: true, min: 6, max: 255 },
+  name: { type: String, required: true, max: 255 },
+  description: { type: String, required: true, max: 255 },
+  file: { type: String, required: true, max: 255 },
   date: { type: Date, default: Date.Now },
   validated: { type: Boolean, default: false }
 });
